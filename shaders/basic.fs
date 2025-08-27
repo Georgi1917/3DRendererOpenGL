14da3591691a5a07eb32c9790966ceed90f51d63
Gosho Zvqra <mamarida15@abv.bg>
2025-08-27 16:42:26 +0300

    further work on lighting

--- shaders/basic.fs
+++ shaders/basic.fs
@@ -4,6 +4,7 @@ out vec4 FragColor;
 uniform vec3 uColor;
 uniform vec3 lColor;
 uniform vec3 lPos;
+uniform vec3 viewPos;
 
 in vec3 Normal;
 in vec3 FragPos;
@@ -11,14 +12,22 @@ in vec3 FragPos;
 void main()
 {
 
-    float ambientStrenght = 0.1;
+    float ambientStrenght = 0.3;
+    float specStrenght = 0.5;
+
     vec3 ambient = lColor * ambientStrenght;
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lColor;
-    vec3 result = (ambient + diffuse) * uColor;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specStrenght * spec * lColor;
+
+    vec3 result = (ambient + diffuse + specular) * uColor;
 
     FragColor = vec4(result, 1.0f);
 
