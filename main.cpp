fde0efe502cc82ebe451fd24809cca1331a6df07
Gosho Zvqr <mamarida15@abv.bg>
2025-11-05 11:00:31 +0200

    fixed main method

--- main.cpp
+++ main.cpp
@@ -198,64 +198,64 @@ int main()
         if (ImGui::Checkbox("Draw Skybox", &renderer.hasSkybox))
         {}
             
-        if (mousePicker.GetClickedObj())
-        {
+    //     if (mousePicker.GetClickedObj())
+    //     {
 
-            if (ImGui::Button("Remove Texture"))
-            {
+    //         if (ImGui::Button("Remove Texture"))
+    //         {
 
-                //mousePicker.GetClickedObj()->tex = nullptr;
+    //             //mousePicker.GetClickedObj()->tex = nullptr;
 
-            }
+    //         }
 
-            if (ImGui::Button("Select Texture"))
-                ImGui::OpenPopup("Textures");
+    //         if (ImGui::Button("Select Texture"))
+    //             ImGui::OpenPopup("Textures");
 
-            if (ImGui::BeginPopup("Textures"))
-            {
+    //         if (ImGui::BeginPopup("Textures"))
+    //         {
 
-                const char *path = "texture-files/";
-                for (const auto &entry : std::filesystem::directory_iterator(path))
-                {
+    //             const char *path = "texture-files/";
+    //             for (const auto &entry : std::filesystem::directory_iterator(path))
+    //             {
 
-                    if(ImGui::MenuItem(entry.path().generic_string().c_str()))
-                    {
+    //                 if(ImGui::MenuItem(entry.path().generic_string().c_str()))
+    //                 {
 
-                        //mousePicker.GetClickedObj()->tex = new Texture(entry.path().generic_string().c_str());
+    //                     //mousePicker.GetClickedObj()->tex = new Texture(entry.path().generic_string().c_str());
 
-                    }
+    //                 }
 
-                }
+    //             }
 
-                ImGui::EndPopup();
+    //             ImGui::EndPopup();
 
-            }
+    //         }
 
-            if (ImGui::Button("Delete Object"))
-            {
+    //         if (ImGui::Button("Delete Object"))
+    //         {
 
-                for (auto it = renderer.models_c.begin(); it != renderer.models_c.end();)
-                {
+    //             for (auto it = renderer.models_c.begin(); it != renderer.models_c.end();)
+    //             {
 
-                    if (*it == mousePicker.GetClickedObj())
-                    {
+    //                 if (*it == mousePicker.GetClickedObj())
+    //                 {
 
-                        delete *it;
-                        it = renderer.models_c.erase(it);
+    //                     delete *it;
+    //                     it = renderer.models_c.erase(it);
 
-                    }
-                    else it++;
+    //                 }
+    //                 else it++;
 
-                }
+    //             }
 
-            }
+    //         }
 
-            ImGui::DragFloat3("Translation", &(mousePicker.GetClickedObj()->trans.x), 1.0f * deltaTime, -100, 100);
-            ImGui::DragFloat3("Rotation", &(mousePicker.GetClickedObj()->rotation.x), 10.0f, -360, 360);
-            ImGui::DragFloat3("Scale", &(mousePicker.GetClickedObj()->scale.x), 2.0f * deltaTime, 1.0f, 100.0f);
-            ImGui::ColorPicker3("Color", &(mousePicker.GetClickedObj()->color.r));
+    //         ImGui::DragFloat3("Translation", &(mousePicker.GetClickedObj()->trans.x), 1.0f * deltaTime, -100, 100);
+    //         ImGui::DragFloat3("Rotation", &(mousePicker.GetClickedObj()->rotation.x), 10.0f, -360, 360);
+    //         ImGui::DragFloat3("Scale", &(mousePicker.GetClickedObj()->scale.x), 2.0f * deltaTime, 1.0f, 100.0f);
+    //         ImGui::ColorPicker3("Color", &(mousePicker.GetClickedObj()->color.r));
             
-        }
+    //     }
         
         ImGui::End();
 
