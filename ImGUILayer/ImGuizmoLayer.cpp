e223ed9f2ad438f2cc5f3c955fc72d1d07784c7d
Gosho Zvqra <mamarida15@abv.bg>
2026-03-07 00:02:04 +0200

    some sht

--- ImGUILayer/ImGuizmoLayer.cpp
+++ ImGUILayer/ImGuizmoLayer.cpp
@@ -1,1 +1,10 @@
 #include "ImGuizmoLayer.h"
+
+void ImGuizmoLayer::BeginFrame(int x, int y, int width, int height)
+{
+
+    ImGuizmo::BeginFrame();
+    ImGuizmo::SetDrawlist(ImGui::GetForegroundDrawList());
+    ImGuizmo::SetRect(x, y, width, height);
+
+}
--- ImGUILayer/ImGuizmoLayer.h
+++ ImGUILayer/ImGuizmoLayer.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "../Scene/Model.h"
+#include "../include/imgui/imgui.h"
+#include "../include/imgui/imgui_impl_glfw.h"
+#include "../include/imgui/imgui_impl_opengl3.h"
+#include "../include/imguizmo/ImGuizmo.h"
 
 struct ImGuizmoLayer
 {
